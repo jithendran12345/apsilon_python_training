a2643ba8aa0e3eb02725c924d3d739a13c317971
jithendran12345 <113332393+jithendran12345@users.noreply.github.com>
2022-09-12 12:50:16 +0530

    Add files via upload

---
+++ program7@activity1.py
@@ -0,0 +1,21 @@
+#question7_activity1
+#a) Declare a variable fname & initialize your first name as its value
+#b) Declare another variable lname & initialize your last name as its value
+#c) using string concatenation operator,join fname with lname and display its les length
+
+#Activity-code: KL/EP-19/A-001
+#Platform: python 3.10,winx 10
+#author name="mr.JITHENDRAN"
+#Role: Software Engineer, Apsilon
+
+
+
+
+
+fname='jithendran'    #declare a variable as 'fname'
+lname='dhanaraj'      #declare a variable as 'lastname' 
+fullname=fname+lname  #using string concatenation operator add both the names
+print('fullname: ',fullname,'\nlength    :',len(fullname))
+
+                      #print the fullname and lenght of the name by using len() 
+
